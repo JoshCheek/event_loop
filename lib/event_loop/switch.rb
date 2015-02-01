fd96511c147d0e732a9e51ec35137ab5145b14e1
Josh Cheek <josh.cheek@gmail.com>
2015-02-01 03:03:27 -0700

    Lock the count with a mutex
    Don't have a failing spec, not really sure how to make one.
    But everything continues to pass, and looking at it, it seems correct.

--- lib/event_loop/switch.rb
+++ lib/event_loop/switch.rb
@@ -1,19 +1,27 @@
 class EventLoop
   class Switch
     def initialize(initial_state)
-      unless states.include? initial_state
+      states.include? initial_state or
         raise ArgumentError, "Unknown initial state: #{initial_state}, expected one of #{states.inspect}"
-      end
-      self.state = initial_state
-      self.queue = Queue.new
-      self.num_blocked = 0 # this might be mutex territory
+      self.state       = initial_state
+      self.queue       = Queue.new
+      self.semaphore   = Mutex.new
+      self.num_blocked = 0
     end
 
     attr_reader :state, :num_blocked
 
+    def blocked?
+      state == :blocked
+    end
+
+    def unblocked?
+      state == :unblocked
+    end
+
     def wait
       return nil if unblocked?
-      @num_blocked += 1
+      semaphore.synchronize { @num_blocked += 1 }
       queue.shift
       nil
     end
@@ -23,27 +31,20 @@ class EventLoop
       :blocked
     end
 
-    def blocked?
-      state == :blocked
-    end
-
     def unblock
       return :unblocked if unblocked?
-      self.state = :unblocked
-      num_blocked = @num_blocked
-      @num_blocked = 0
-      num_blocked.times { queue << nil }
+      semaphore.synchronize do
+        self.state = :unblocked
+        num_blocked.times { queue << nil }
+        self.num_blocked = 0
+      end
       :unblocked
-    end
-
-    def unblocked?
-      state == :unblocked
     end
 
     private
 
     attr_writer :state, :num_blocked
-    attr_accessor :queue
+    attr_accessor :queue, :semaphore
 
     def states
       @states ||= [:blocked, :unblocked]
